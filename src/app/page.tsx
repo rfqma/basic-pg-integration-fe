26e1767f4aecc36b0f6ab04f4e56699e1ff7d285
Rifqi Maulana <abelcaster025@gmail.com>
2023-10-11 19:30:29 +0700

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { useEffect, useState } from "react"
+
+export default function HomePage() {
+  const [name, setName] = useState<string>('')
+  const [orderId, setOrderId] = useState<string>('')
+  const [total, setTotal] = useState<string>('')
+  const [token, setToken] = useState<string>('')
+
+  const handleProcess = async () => {
+    const dataPayment = {
+      name: name,
+      order_id: orderId,
+      total: parseInt(total)
+    }
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/payment/process`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ...dataPayment })
+    })
+
+    const data = await response.json();
+    setToken(data.token)
+  }
+
+  useEffect(() => {
+    if (token) {
+      (window as any).snap.pay(token, {
+        onSuccess: (result: any) => {
+          localStorage.setItem('Payment', JSON.stringify(result))
+          console.log('success', result)
+          setToken('')
+        },
+        onPending: (result: any) => {
+          localStorage.setItem('Payment', JSON.stringify(result))
+          console.log('pending', result)
+          setToken('')
+        },
+        onError: (error: any) => {
+          console.log('error', error)
+          setToken('')
+        },
+        onClose: () => {
+          console.log('You close the popup without finishing the payment')
+          setToken('')
+        }
+      })
+
+      setName('')
+      setOrderId('')
+      setTotal('')
+    }
+  }, [token])
+
+  useEffect(() => {
+    const midtransSnapUrl = 'https://app.sandbox.midtrans.com/snap/snap.js'
+
+    const scriptTag = document.createElement('script')
+    scriptTag.src = midtransSnapUrl
+
+    const midtransClientKey = process.env.MIDTRANS_SANDBOX_CLIENT_KEY
+    scriptTag.setAttribute('data-client-key', midtransClientKey!)
+
+    document.body.appendChild(scriptTag)
+
+    return () => {
+      document.body.removeChild(scriptTag)
+    }
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="flex justify-center h-screen">
+        <div className="w-1/2">
+          <div className="flex flex-col w-full gap-5 p-5">
+            <input
+              type="text"
+              aria-label="Name"
+              value={name}
+              onChange={(event) => { setName(event.target.value) }}
+              className="p-2 text-sm border border-gray-400 rounded"
             />
-          </a>
+            <input
+              type="text"
+              aria-label="Order ID"
+              value={orderId}
+              onChange={(event) => { setOrderId(event.target.value) }}
+              className="p-2 text-sm border border-gray-400 rounded"
+            />
+            <input
+              type="number"
+              aria-label="Total"
+              value={total}
+              onChange={(event) => { setTotal(event.target.value) }}
+              className="p-2 text-sm border border-gray-400 rounded"
+            />
+
+            <button
+              onClick={handleProcess}
+              className="p-2 bg-black rounded"
+            >
+              <span className="text-sm text-white">Proceed</span>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
